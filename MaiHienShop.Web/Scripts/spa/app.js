bd72120ef1e08ad3acf6998c39d50f46e4be5008
LuanNV <luanvne@outlook.com>
2018-07-04 14:31:53 +0700

    service in angularjs

--- MaiHienShop.Web/Scripts/spa/app.js
+++ MaiHienShop.Web/Scripts/spa/app.js
@@ -2,28 +2,50 @@
 
 var myApp = angular.module('myModule', []);
 
-myApp.controller("myControllerName", myControllerFunc);
+//myApp.controller("myControllerName", myControllerFunc);
 myApp.controller("schoolController", schoolController);
-myApp.controller("teacherController", teacherController);
-myApp.controller("studentController", studentController);
+myApp.service('ValidatorServiceName', ValidatorServiceImpl);
+
+schoolController.$inject = ['$scope', 'ValidatorServiceName'];
+
+//myApp.controller("teacherController", teacherController);
+//myApp.controller("studentController", studentController);
 
 //myControllerFunc.$inject = ['$scope'];
 //teacherController.$inject = ['$scope'];
 //studentController.$inject = ['$scope'];
 
 //declare
-function myControllerFunc($scope) {
-    $scope.message = "This is my message from Controller";
+//function myControllerFunc($scope) {
+//    $scope.message = "This is my message from Controller";
+//}
+
+function schoolController($scope, ValidatorServiceName) {
+    //$scope.message = "Announcement from school";
+    $scope.clickCheckNumber = function () {
+        $scope.message = ValidatorServiceName.returnCheckNumber($scope.num);
+    }
+
+    $scope.num = 1;
 }
 
-function schoolController($scope) {
-    $scope.message = "Announcement from school";
-}
+//function teacherController($scope) {
+//    $scope.message = "This is my message from Teacher";
+//}
 
-function teacherController($scope) {
-    //$scope.message = "This is my message from Teacher";
-}
+//function studentController($scope) {
+//    $scope.message = "This is my message from Student";
+//}
 
-function studentController($scope) {
-    $scope.message = "This is my message from Student";
+function ValidatorServiceImpl($window) {
+    return {
+        returnCheckNumber: checkNumber
+    }
+    function checkNumber(input) {
+        if (input % 2 == 0) {
+            return 'This is even';
+        }
+        else
+            return 'This is odd';
+    }
 }
