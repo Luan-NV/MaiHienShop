4cf89853098a97ce8189fd051759a75752b0f92f
LuanNV <luanvne@outlook.com>
2018-07-06 14:14:02 +0700

    paging for list

--- MaiHienShop.Web/app/components/product_categories/productCategoryListController.js
+++ MaiHienShop.Web/app/components/product_categories/productCategoryListController.js
@@ -5,17 +5,35 @@
 
     function productCategoryListController($scope, apiService) {
         $scope.productCategories = [];
+        $scope.page = 0;
+        $scope.pagesCount = 0;
+        $scope.getProductCagories = getProductCagories;
+        $scope.keyword = '';
 
-        $scope.getProductCategories = getProductCategories;
+        $scope.search = search;
 
-        function getProductCategories() {
-            apiService.get('/api/productcategory/getall', null, function (result) {
-                $scope.productCategories = result.data;
+        function search() {
+            getProductCagories();
+        }
+        function getProductCagories(page) {
+            page = page || 0;
+            var config = {
+                params: {
+                    keyword: $scope.keyword,
+                    page: page,
+                    pageSize: 2
+                }
+            }
+            apiService.get('/api/productcategory/getall', config, function (result) {
+                $scope.productCategories = result.data.Items;
+                $scope.page = result.data.Page;
+                $scope.pagesCount = result.data.TotalPages;
+                $scope.totalCount = result.data.TotalCount;
             }, function () {
                 console.log('Load productcategory failed.');
             });
         }
 
-        $scope.getProductCategories();
+        $scope.getProductCagories();
     }
 })(angular.module('maihienshop.product_categories'));
